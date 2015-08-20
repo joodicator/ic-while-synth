3c8d13838e84cc8ded81d4c349d9dc8fc22cde72
Joseph Crowe <jjc311@ic.ac.uk>
2015-08-20 22:44:29 +0100

    Fix errors in run_common.lp and elsewhere causing a large grounding. Update the ASP grounding profiler to show distribution of rule bodies.

--- main/ASP/Parse.hs
+++ main/ASP/Parse.hs
@@ -1,5 +1,6 @@
-{-# LANGUAGE Rank2Types, TypeFamilies, FlexibleContexts, TransformListComp,
-             OverloadedStrings #-}
+{-# LANGUAGE Rank2Types, TypeFamilies, FlexibleContexts,
+             FlexibleInstances, UndecidableInstances,
+             MultiParamTypeClasses, OverloadedStrings, TransformListComp #-}
 
 module ASP.Parse where
 
@@ -11,12 +12,12 @@ import GHC.Exts(sortWith, groupWith)
 import Text.Parsec
 
 import Prelude hiding (head)
+import Data.ByteString (ByteString)
 import Data.Either
 import Data.Monoid
+import Data.Functor.Identity
 import Control.Applicative hiding ((<|>), many)
 import Control.Monad
-
-type Parse s u m a = Stream s m Char => ParsecT s u m a
 
 opInfo' :: (Operator a, OpContext a ~ ASPContext) => a -> OpInfo
 opInfo' = opInfo Clingo3
@@ -24,138 +25,140 @@ opInfo' = opInfo Clingo3
 --------------------------------------------------------------------------------
 -- Parsing of ASP structures.
 
-rule :: Parse s u m Rule
-rule = (mempty :-) <$> (pre ":-" *> (mempty <$ char '.' <|> body <* suf "."))
-   <|> (:-) <$> head <*> (inf ":-" *> (body <* suf "." <|> mempty <$ char '.')
-   <|> mempty <$ suf ".")
+class ParseASP s u m where
+    rule       :: ParsecT s u m Rule
+    head       :: ParsecT s u m Head
+    body       :: ParsecT s u m Body
+    atom       :: ParsecT s u m Atom
+    conjunct   :: ParsecT s u m Conjunct
+    literal    :: ParsecT s u m Literal
+    comparison :: ParsecT s u m Comparison
+    expr       :: ParsecT s u m Expr
+    cBiOp      :: ParsecT s u m CBiOp
+    eUnOp      :: EUnOp -> ParsecT s u m EUnOp
+    eBiOp      :: EBiOp -> ParsecT s u m EBiOp
+    term       :: ParsecT s u m Term
+    intTerm    :: ParsecT s u m Integer
+    strTerm    :: ParsecT s u m String
+    variable   :: ParsecT s u m Variable
+    lName      :: (String -> a) -> ParsecT s u m a
+    args       :: ParsecT s u m a -> ParsecT s u m [a]
+    inf        :: String -> ParsecT s u m ()
+    pre        :: String -> ParsecT s u m ()
+    suf        :: String -> ParsecT s u m ()
 
-head :: Parse s u m Head
-head = Head <$> sepBy1 atom (try $ inf "|")
+instance (Monad m, Stream s m Char) => ParseASP s u m where
+    {-# SPECIALISE instance ParseASP String () Identity #-}
 
-body :: Parse s u m Body
-body = Body <$> sepBy1 conjunct (inf ",")
+    rule = (mempty :-) <$> (pre ":-" *> (mempty <$ char '.' <|> body <* suf "."))
+       <|> (:-) <$> head <*> (inf ":-" *> (body <* suf "." <|> mempty <$ char '.')
+       <|> mempty <$ suf ".")
+        
+    head = Head <$> sepBy1 atom (try $ inf "|")
+    
+    body = Body <$> sepBy1 conjunct (inf ",")
+    
+    atom = Atom <$> lName Predicate <*> args expr
+    
+    conjunct = CLiteral   <$> literal
+           <|> CAssign    <$> variable <* inf "=" <*> expr
+           <|> CCondition <$> literal  <* inf ":" <*> conjunct
+    
+    literal = LCompare <$> try comparison
+          <|> LNot     <$> (try (string "not" *> many1 space) *> atom)
+          <|> LAtom    <$> atom
+    
+    comparison = flip CBiOp <$> expr <* spaces <*> cBiOp <* spaces <*> expr
+    
+    cBiOp = CEq <$ string "==" <|> CNE <$ string "!="
+        <|> CLE <$ string "<=" <|> CGT <$ string ">"
+        <|> CGE <$ string ">=" <|> CLT <$ string "<"
+    
+    expr = expr' oss where
+        --oss :: [[Either EUnOp EBiOp]]
+        oss = [o | o <- map Left  [minBound::EUnOp ..]
+                     ++ map Right [minBound::EBiOp ..],
+                   let p = oPrec $ either opInfo' opInfo' o,
+                   then sortWith by p, then group by p using groupWith]
+    
+        --expr' :: [[Either EUnOp EBiOp]] -> ParsecT s u m Expr
+        expr' [] = choice
+           [try $ between (pre "(") (suf ")") expr,
+            EUnOp EAbs <$> between (pre "|") (suf "|") expr,
+            ETerm <$> term]
+        expr' oss'@(os@(_:_) : _) = choice
+           [exprUn (lefts  os) oss',
+            exprBi (rights os) oss']
+        expr' ([] : _) = error "impossible"
+    
+        --exprUn :: [EUnOp] -> [[Either EUnOp EBiOp]] -> ParsecT s u m Expr
+        exprUn os oss' = do
+            o <- choice $ map eUnOp os
+            let OpInfo{ oAscL=ascL } = opInfo' o
+            x <- expr' $ if ascL then oss' else drop 1 oss'
+            return $ EUnOp o x
+    
+        --exprBi :: [EBiOp] -> [[Either EUnOp EBiOp]] -> ParsecT s u m Expr
+        exprBi os oss' = do
+            x  <- expr' $ drop 1 oss'
+            mo <- optionMaybe . choice $ map eBiOp os
+            case mo of
+                Just o | oAscL (opInfo' o) -> exprAscL x o os oss'
+                Just o | oAscR (opInfo' o) -> EBiOp o x <$> expr' oss'
+                Just o | otherwise         -> EBiOp o x <$> expr' (drop 1 oss')
+                Nothing                    -> return x
+    
+        --exprAscL
+        --   :: Expr -> EBiOp -> [EBiOp] -> [[Either EUnOp EBiOp]] -> ParsecT s u m Expr
+        exprAscL x o os oss' = do
+            x' <- EBiOp o x <$> expr' (drop 1 oss')
+            mo <- optionMaybe . choice $ map eBiOp os
+            case mo of
+                Just o' | oAscL (opInfo' o) -> exprAscL x' o' os oss'
+                Just _  | otherwise        -> fail "ambiguous arithmetic expression"
+                Nothing                    -> return x'
+    
+    eUnOp op = case op of
+        EAbs -> op <$ try (string "#abs" *> notFollowedBy (alphaNum <|> char '_'))
+        ENeg -> op <$ char '-'
+        ENot -> op <$ char '~'
+    
+    eBiOp op = case op of
+        EPow -> op <$ try (string "**")
+        EMod -> op <$ char '\\'
+        EDiv -> op <$ char '/'
+        EMul -> op <$ char '*'
+        ESub -> op <$ char '-'
+        EAdd -> op <$ char '+'
+        EAnd -> op <$ char '&'
+        EOr  -> op <$ char '?'
+        EXOr -> op <$ char '^'
+    
+    term = TVar <$> variable
+       <|> TInt <$> intTerm
+       <|> TStr <$> strTerm
+       <|> option (TFun "") (lName $ TFun . Function) <*> args expr
+       <|> char '@' *> lName (TLua . LuaFunc) <*> args expr
+    
+    intTerm = read <$> many1 digit
+    
+    strTerm = between (char '"') (char '"')
+              (many $ char '\\' *> char '"' <|> noneOf "\"\\")
 
-atom :: Parse s u m Atom
-atom = Atom <$> lName Predicate <*> args expr
-
-conjunct :: Parse s u m Conjunct
-conjunct = CLiteral   <$> literal
-       <|> CAssign    <$> variable <* inf "=" <*> expr
-       <|> CCondition <$> literal  <* inf ":" <*> conjunct
-
-literal :: Parse s u m Literal
-literal = try (LCompare <$> comparison)
-      <|> LAtom    <$> atom
-      <|> LNot     <$> (string "not" *> many1 space *> atom)
-
-comparison :: Parse s u m Comparison
-comparison = flip CBiOp <$> expr <* spaces <*> cBiOp <* spaces <*> expr
-
-cBiOp :: Parse s u m CBiOp
-cBiOp = CEq <$ string "==" <|> CNE <$ string "!="
-    <|> CLE <$ string "<=" <|> CGT <$ string ">"
-    <|> CGE <$ string ">=" <|> CLT <$ string "<"
-
-expr :: Parse s u m Expr
-expr = expr' oss where
-    oss :: [[Either EUnOp EBiOp]]
-    oss = [o | o <- map Left  [minBound::EUnOp ..]
-                 ++ map Right [minBound::EBiOp ..],
-               let p = oPrec $ either opInfo' opInfo' o,
-               then sortWith by p, then group by p using groupWith]
-
-    expr' :: [[Either EUnOp EBiOp]] -> Parse s u m Expr
-    expr' [] = choice
-       [try $ between (pre "(") (suf ")") expr,
-        EUnOp EAbs <$> between (pre "|") (suf "|") expr,
-        ETerm <$> term]
-    expr' oss'@(os@(_:_) : _) = choice
-       [exprUn (lefts  os) oss',
-        exprBi (rights os) oss']
-    expr' ([] : _) = error "impossible"
-
-    exprUn :: [EUnOp] -> [[Either EUnOp EBiOp]] -> Parse s u m Expr
-    exprUn os oss' = do
-        o <- choice $ map eUnOp os
-        let OpInfo{ oAscL=ascL } = opInfo' o
-        x <- expr' $ if ascL then oss' else drop 1 oss'
-        return $ EUnOp o x
-
-    exprBi :: [EBiOp] -> [[Either EUnOp EBiOp]] -> Parse s u m Expr
-    exprBi os oss' = do
-        x  <- expr' $ drop 1 oss'
-        mo <- optionMaybe . choice $ map eBiOp os
-        case mo of
-            Just o | oAscL (opInfo' o) -> exprAscL x o os oss'
-            Just o | oAscR (opInfo' o) -> EBiOp o x <$> expr' oss'
-            Just o | otherwise         -> EBiOp o x <$> expr' (drop 1 oss')
-            Nothing                    -> return x
-
-    exprAscL
-       :: Expr -> EBiOp -> [EBiOp] -> [[Either EUnOp EBiOp]] -> Parse s u m Expr
-    exprAscL x o os oss' = do
-        x' <- EBiOp o x <$> expr' (drop 1 oss')
-        mo <- optionMaybe . choice $ map eBiOp os
-        case mo of
-            Just o' | oAscL (opInfo' o) -> exprAscL x' o' os oss'
-            Just _  | otherwise        -> fail "ambiguous arithmetic expression"
-            Nothing                    -> return x'
-
-eUnOp :: EUnOp -> Parse s u m EUnOp
-eUnOp op = case op of
-    EAbs -> op <$ try (string "#abs" *> notFollowedBy (alphaNum <|> char '_'))
-    ENeg -> op <$ char '-'
-    ENot -> op <$ char '~'
-
-eBiOp :: EBiOp -> Parse s u m EBiOp
-eBiOp op = case op of
-    EPow -> op <$ try (string "**")
-    EMod -> op <$ char '\\'
-    EDiv -> op <$ char '/'
-    EMul -> op <$ char '*'
-    ESub -> op <$ char '-'
-    EAdd -> op <$ char '+'
-    EAnd -> op <$ char '&'
-    EOr  -> op <$ char '?'
-    EXOr -> op <$ char '^'
-
-term :: Parse s u m Term
-term = TVar <$> variable
-   <|> TInt <$> intTerm
-   <|> TStr <$> strTerm
-   <|> option (TFun "") (lName $ TFun . Function) <*> args expr
-   <|> char '@' *> lName (TLua . LuaFunc) <*> args expr
-
-intTerm :: Parse s u m Integer
-intTerm = read <$> many1 digit
-
-strTerm :: Parse s u m String
-strTerm = between (char '"') (char '"')
-          (many $ char '\\' *> char '"' <|> noneOf "\"\\")
-
---------------------------------------------------------------------------------
--- Parsing of low-level ASP tokens:
-
-variable :: Parse s u m Variable
-variable = fmap Variable $ (:) <$> (upper <|> char '_')
-                               <*> many (alphaNum <|> char '_')
-
--- The name of a predicate, function symbol or constant.
-lName :: (String -> a) -> Parse s u m a
-lName con = fmap con $ (:) <$> lower <*> many (alphaNum <|> char '_')
-
--- A parenthesised list of 1 or more arguments, or the empty string.
-args :: Parse s u m a -> Parse s u m [a]
-args arg = option [] . between (pre "(") (suf ")") $ sepBy1 arg (inf ",")
-
--- Infix operators (may follow or be followed by space):
-inf :: String -> Parse s u m ()
-inf op = void $ spaces *> string op *> spaces
-
--- Prefix operators (may be followed by space):
-pre :: String -> Parse s u m ()
-pre op = void $ string op *> spaces
-
--- Suffix operators (may follow space):
-suf :: String -> Parse s u m ()
-suf op = void $ spaces *> string op
+    variable = fmap Variable $ (:) <$> (upper <|> char '_')
+                                   <*> many (alphaNum <|> char '_')
+    
+    -- The name of a predicate, function symbol or constant.
+    lName con = fmap con $ (:) <$> lower <*> many (alphaNum <|> char '_')
+    
+    -- A parenthesised list of 1 or more arguments, or the empty string.
+    args arg = option [] . between (pre "(") (suf ")") $ sepBy1 arg (inf ",")
+    
+    -- Infix operators (may follow or be followed by space):
+    inf op = void $ spaces *> string op *> spaces
+    
+    -- Prefix operators (may be followed by space):
+    pre op = void $ string op *> spaces
+    
+    -- Suffix operators (may follow space):
+    suf op = void $ spaces *> string op
--- main/Profile.hs
+++ main/Profile.hs
@@ -1,57 +1,121 @@
 #!/usr/bin/env runhaskell
 
-{-# LANGUAGE ViewPatterns, FlexibleContexts #-}
+{-# LANGUAGE ViewPatterns, BangPatterns, FlexibleContexts,
+             OverloadedStrings #-}
 
 module Main where
 
-import ASP
-import qualified ASP.Parse as Parse
-import Text.Parsec
+import qualified Data.Attoparsec.ByteString.Char8 as A
 
 import qualified Data.Map.Strict as M
-import qualified Data.Set as S
+import Data.Map.Strict (Map)
+
+import qualified Data.ByteString.Char8 as B
+import Data.ByteString.Char8 (ByteString)
+
 import Data.List
-import Data.Monoid
-import Control.Applicative hiding ((<|>))
 import Control.Monad
+import Control.Applicative
 import System.IO
-import System.Exit
 
-parseHead :: Parse.Parse s u m Head
-parseHead
-  = option mempty Parse.head <* (string "." <|> string ":-") <* skipMany anyToken
+type Count = Int
+type Pred  = ByteString
+type Arity = Int
+type HeadK = [(Pred, Arity)]
+type BodyK = [(Pred, Arity, Bool)]
 
+main :: IO ()
 main = main' M.empty
 
-main' :: M.Map (Maybe (S.Set (String, Int))) Int -> IO ()
-main' counts = do
-    eof <- isEOF
+main' :: Map (Maybe HeadK) (Count, Map BodyK Count) -> IO ()
+main' !counts = do
+    !eof <- isEOF
     case eof of
       False -> do
-        line <- getLine
-        let result = parse parseHead "" line
-        key <- case result of
-            Right (Head hs) -> do
-                return . Just . S.fromList $ map key hs
-            Left _ -> do
-                return Nothing
-        main' $! M.insertWith (+) key 1 counts
+        !line <- B.getLine
+        case A.parseOnly rule line of
+            !(Right !(!hKey, !bKey)) -> do
+                let !(!count, !bMap) = M.findWithDefault (0, M.empty) (Just hKey) counts
+                main' $! M.insert (Just hKey) (count+1, M.insertWith (+) bKey 1 bMap) counts
+            !(Left _) -> do
+                let !(!count, !bMap) = M.findWithDefault (0, M.empty) Nothing counts
+                main' $! M.insert Nothing (count+1, bMap) counts
       True ->
-        main'' counts
+        main'' $! counts
 
-main'' :: M.Map (Maybe (S.Set (String, Int))) Int -> IO ()
-main'' counts
-  = forM_ (sortBy (compare `on` snd) (M.toList counts)) $ \(k, v) -> do
-        ks <- return $ case k of
-            Nothing               -> "unknown"
-            Just (S.toList -> hs) -> keys hs
-        putStrLn $ show v ++ "\t" ++ ks
+main'' :: Map (Maybe HeadK) (Count, Map BodyK Count) -> IO ()
+main'' !counts = do
+    let sortedHeads = sortBy (compare `on` (fst . snd)) (M.toList counts)
+    forM_ sortedHeads $ \(mhKey, (count, bMap)) -> do
+        B.putStr $ B.pack (show count) +++ "\t" +++ case mhKey of
+            Nothing   -> "unknown"
+            Just hKey -> showHKey hKey
+        case M.toList bMap of
+            [([], _)] -> do
+                B.putStrLn "."
+            [(bKey@(_:_), _)] -> do
+                B.putStrLn $ " " +++ showBKey bKey
+            _ -> do
+                B.putStrLn ""
+                let sortedBodies = sortBy (compare `on` snd) $ M.toList bMap
+                forM_ sortedBodies $ \(bKey, bCount) -> do
+                    B.putStrLn $ "\t" +++ B.pack (show bCount) +++ "\t" +++ showBKey bKey
   where
-    on f g x y = f (g x) (g y)
+    on f g x y  = f (g x) (g y)
 
-key :: Atom -> (String, Int)
-key (Atom (Predicate name) args) = (name, length args)
+--------------------------------------------------------------------------------
+-- Parsing of ground rules:
 
-keys :: [(String, Int)] -> String
-keys [] = "/0"
-keys ks = intercalate " | " [p ++"/"++ show n | (p,n) <- ks]
+rule :: A.Parser (HeadK, BodyK)
+rule = (,) <$> headK <*> (A.string ":-" *> bodyK1 <|> pure []) <* A.char '.'
+
+headK :: A.Parser HeadK
+headK = headK1 <|> pure []
+
+headK1 :: A.Parser HeadK
+headK1 = (:) <$> predArity <*> (A.char '|' *> headK1 <|> pure [])
+
+bodyK1 :: A.Parser BodyK
+bodyK1 = (:) <$> atom <*> (A.char ',' *> bodyK1 <|> pure [])
+
+atom :: A.Parser (Pred, Arity, Bool)
+atom = do void (A.string "not "); (p,a) <- predArity; return (p,a,False)
+   <|> do                         (p,a) <- predArity; return (p,a,True)
+
+predArity :: A.Parser (Pred, Arity)
+predArity = (,) <$> predName <*> (A.char '(' *> args1 <* A.char ')' <|> pure 0)
+
+args1 :: A.Parser Arity
+args1 = (+ 1) <$> (term *> (A.char ',' *> args1 <|> pure 0))
+
+predName :: A.Parser Pred
+predName
+  = B.cons <$> A.letter_ascii <*> A.takeWhile isPred
+  where
+    isPred c = A.isAlpha_ascii c || A.isDigit c || c == '_'
+
+term :: A.Parser ()
+term = ter where
+    ter  = A.skipMany $ par <|> str <|> A.satisfy (A.notInClass ",)")
+    ter' = A.skipMany $ par <|> str <|> A.notChar ')'
+    par  = A.char '(' *> ter' *> A.char ')'
+    str  = A.char '"' *> str'
+    str' = A.char '"' <|> (esc <|> A.anyChar) *> str'
+    esc  = A.char '\\' *> A.anyChar
+
+--------------------------------------------------------------------------------
+-- Miscellaneous utilities:
+
+showHKey :: HeadK -> ByteString
+showHKey [] = "/0"
+showHKey ps = B.intercalate " | " [
+    p +++ "/" +++ B.pack (show a) | (p,a) <- ps]
+
+showBKey :: BodyK -> ByteString
+showBKey ps = ":- " +++ B.intercalate ", " [
+    (if b then "" else "not ") +++ p +++ "/" +++ B.pack (show a)
+    | (p,a,b) <- ps] +++ "."
+
+infixr 5 +++
+(+++) :: ByteString -> ByteString -> ByteString
+(+++) = B.append
